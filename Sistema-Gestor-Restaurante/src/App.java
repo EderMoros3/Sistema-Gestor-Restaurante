2e44380675b9cf30a3367696a7ee7c086fa49f50
Eder Moros <edermorosrodero@gmail.com>
2025-03-17 13:06:41 +0100

    Creacion de Menu

--- Sistema-Gestor-Restaurante/src/App.java
+++ Sistema-Gestor-Restaurante/src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\nMenu de Gestion de Restaurante");
+            System.out.println("1. Registrar Mesa");
+            System.out.println("2. Registrar Plato");
+            System.out.println("3. Registrar Pedido");
+            System.out.println("4. Modificar Pedido");
+            System.out.println("5. Borrar Pedido");
+            System.out.println("6. Modificar o Borrar Plato");
+            System.out.println("7. Guardar Datos");
+            System.out.println("8. Cargar Datos");
+            System.out.println("9. Salir");
+            System.out.println("Seleccione una opcion");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1 -> {
+                    //registarMesa();
+                }
+                case 2 -> {
+                    //registrarPlato();
+                }
+                case 3 -> {
+                    //registrarPedido();
+                }
+                case 4 -> {
+                    //modificarPedido();
+                }
+                case 5 -> {
+                    //borrarPedido();
+                }
+                case 6 -> {
+                    //modificarOBorrarPlato();
+                }
+                case 7 -> {
+                    //guardarDatos();
+                }
+                case 8 -> {
+                    //cargarDatos();
+                }
+                case 9 -> {
+                    System.out.println("Saliendo del sistema...");
+                }
+                default -> {
+                    System.out.println("Opcion no valida");
+                }
+            }
+        } while (opcion != 9);
     }
 }
